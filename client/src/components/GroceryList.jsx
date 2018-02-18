9e1c42cc660dd0ac63c68a2e74bda69f59f056ce
Annisa <annisakaraca@gmail.com>
2018-02-18 15:29:07 -0800

    Render GroceryList component with values passed down from App

--- client/src/components/GroceryList.jsx
+++ client/src/components/GroceryList.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import GroceryItem from './GroceryItem.jsx';
 
-const GroceryList = (props) => (
-  <div className="groceries"></div>
-)
+const GroceryList = (props) => {
+  let list = props.list;
+  let listItems = list.map((item) => 
+    <GroceryItem item={item} />
+  );
+
+  return (
+    <div className="groceries">
+      {listItems}
+    </div>
+  );
+}
 
 export default GroceryList;
