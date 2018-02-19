bbfcca1320657bd333cba716f0271d34cefb3d44
Annisa <annisakaraca@gmail.com>
2018-02-18 16:04:07 -0800

    Return stubbed data on GET /list

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import GroceryList from './components/GroceryList.jsx';
 import AddGrocery from './components/AddGrocery.jsx'
 
@@ -8,14 +9,23 @@ class App extends React.Component {
     super(props);
     this.state = {
       list: [
-        {id: 1, quantity: 5, description: "frozen pizza"},
-        {id: 2, quantity: 10, description: "greek yogurt"},
-        {id: 3, quantity: 2, description: "wine"},
-        {id: 4, quantity: 1, description: "iced coffee"}
+        // {id: 1, quantity: 5, description: "frozen pizza"},
+        // {id: 2, quantity: 10, description: "greek yogurt"},
+        // {id: 3, quantity: 2, description: "wine"},
+        // {id: 4, quantity: 1, description: "iced coffee"}
       ]
     }
   }
 
+  componentWillMount() {
+    axios.get('/list')
+      .then((response) => {
+        this.setState({list: response.data});
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
   
   render () {
     return (
--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
+// var exampleData = require('../database/data.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/list', function(req, res) {
+  let exampleData = [
+    {id: 1, quantity: 5, description: "frozen pizza"},
+    {id: 2, quantity: 10, description: "greek yogurt"},
+    {id: 3, quantity: 2, description: "wine"},
+    {id: 4, quantity: 1, description: "iced coffee"}
+  ];
+  res.send(exampleData);
+})
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
